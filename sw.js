8e88244b5d1c920826b2f62109744c8ea17a4b92
helderholanda <heldergomesholanda@gmail.com>
2026-07-13 08:35:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,65 +1,110 @@
-const CACHE_NAME = "esp32-ble-controller-v1";
+const CACHE_NAME = "esp32-ble-controller-v2";
+
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./sw.js",
+
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+
 ];
 
+
+
 // Instala o Service Worker
+
 self.addEventListener("install", (event) => {
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
 
-  self.skipWaiting();
+    event.waitUntil(
 
-});
+        caches.open(CACHE_NAME)
 
-// Ativa o Service Worker
-self.addEventListener("activate", (event) => {
+        .then((cache)=>{
 
-  event.waitUntil(
-
-    caches.keys().then((keys) => {
-
-      return Promise.all(
-
-        keys.map((key) => {
-
-          if (key !== CACHE_NAME) {
-
-            return caches.delete(key);
-
-          }
+            return cache.addAll(FILES_TO_CACHE);
 
         })
 
-      );
+    );
 
-    })
 
-  );
+    self.skipWaiting();
 
-  self.clients.claim();
 
 });
 
-// Intercepta as requisições
-self.addEventListener("fetch", (event) => {
 
-  event.respondWith(
 
-    caches.match(event.request)
 
-      .then((response) => {
 
-        return response || fetch(event.request);
+// Ativa e remove caches antigos
 
-      })
+self.addEventListener("activate", (event)=>{
 
-  );
+
+    event.waitUntil(
+
+        caches.keys()
+
+        .then((cacheNames)=>{
+
+
+            return Promise.all(
+
+                cacheNames.map((cache)=>{
+
+
+                    if(cache !== CACHE_NAME){
+
+                        return caches.delete(cache);
+
+                    }
+
+
+                })
+
+            );
+
+
+        })
+
+    );
+
+
+    self.clients.claim();
+
+
+});
+
+
+
+
+
+// Controle das requisições
+
+self.addEventListener("fetch",(event)=>{
+
+
+    event.respondWith(
+
+
+        caches.match(event.request)
+
+        .then((response)=>{
+
+
+            return response || fetch(event.request);
+
+
+        })
+
+
+    );
+
 
 });
