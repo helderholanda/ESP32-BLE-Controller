ff5e556394387b3d123fb7e0bf79c1a44483b073
helderholanda <heldergomesholanda@gmail.com>
2026-07-13 08:26:20 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = "esp32-ble-controller-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Instala o Service Worker
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+
+});
+
+// Ativa o Service Worker
+self.addEventListener("activate", (event) => {
+
+  event.waitUntil(
+
+    caches.keys().then((keys) => {
+
+      return Promise.all(
+
+        keys.map((key) => {
+
+          if (key !== CACHE_NAME) {
+
+            return caches.delete(key);
+
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+// Intercepta as requisições
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then((response) => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
